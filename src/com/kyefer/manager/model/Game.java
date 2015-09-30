24fb60cd215ed9ec41143520cca7ccc92a2c5b3b
Eddie Pyne <empyne16@gmail.com>
2015-09-30 19:31:28 -0400

    Initial Commit/GUI

---
+++ src/com/kyefer/manager/model/Game.java
@@ -0,0 +1,36 @@
+package com.kyefer.manager.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Eddie on 9/30/2015.
+ */
+public class Game {
+    private final StringProperty nameProperty;
+    private List<String> genres;
+
+    public Game(String name){
+        nameProperty = new SimpleStringProperty(name);
+        genres = new ArrayList<>();
+    }
+
+    public String getName(){
+        return nameProperty.get();
+    }
+
+    public StringProperty nameProperty() {
+        return nameProperty;
+    }
+
+    public void addGenre(String genre){
+        genres.add(genre);
+    }
+
+    public List<String> getGenres(){
+        return genres;
+    }
+}
---
+++ src/com/kyefer/manager/view/GameOverviewController.java
@@ -0,0 +1,58 @@
+package com.kyefer.manager.view;
+
+import com.kyefer.manager.Main;
+import com.kyefer.manager.model.Game;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.GridPane;
+
+import javax.xml.soap.Node;
+
+
+public class GameOverviewController {
+    @FXML
+    private TableView<Game> gameTable;
+    @FXML
+    private TableColumn<Game, String> nameColumn;
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private GridPane genreGrid;
+
+    private Main main;
+
+
+    public GameOverviewController() {
+    }
+
+    @FXML
+    private void initialize() {
+        nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+
+        showGenres(null);
+
+        gameTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showGenres(newValue));
+    }
+
+    public void setMainApp(Main main) {
+        this.main = main;
+
+        gameTable.setItems(main.getPersonData());
+    }
+
+    private void showGenres(Game game) {
+        if (game != null) {
+            nameLabel.setText(game.getName());
+            int row = 0;
+            for (String genre : game.getGenres()) {
+                Label genreLabel = new Label(genre);
+                genreGrid.add(genreLabel, 0, row++);
+            }
+        } else {
+            nameLabel.setText("");
+            genreGrid.getChildren().clear();
+        }
+    }
+}
